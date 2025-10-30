04ffa578b9fc60728ac9c1a386afafcd29adc7c0
Manisha Mardi <mardimanisha101@gmail.com>
2025-10-30 15:06:33 +0530

    Updated the metadata
    Signed-off-by: Manisha Mardi <mardimanisha101@gmail.com>

--- app/api/mood-entries/[id]/route.ts
+++ app/api/mood-entries/[id]/route.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { NextRequest, NextResponse } from "next/server";
 import { createSupabaseAdminClient } from "../../../../src/supabase/server/adminClient";
 
